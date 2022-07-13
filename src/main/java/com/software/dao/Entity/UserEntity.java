dacef074f891646765a7dc041ec794c534a27115
Tenderyi <1>
2022-07-13 20:59:33 +0800

    user

--- src/main/java/com/software/dao/Entity/UserEntity.java
+++ src/main/java/com/software/dao/Entity/UserEntity.java
@@ -1,4 +1,36 @@
 package com.software.dao.Entity;
 
 public class UserEntity {
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public UserEntity() {}
+
+    public UserEntity(UserEntity userEntity) {
+        super();
+        this.id = userEntity.getId();
+        this.username = userEntity.getUsername();
+        this.password = userEntity.getPassword();
+    }
 }
