581712138f374f898d24b191d2bcf1a9fe11ae41
Tenderyi <1>
2022-07-14 22:41:13 +0800

    cook

---
+++ target/classes/static/pages/js/loginCheck.js
@@ -0,0 +1,27 @@
+function LoginCheck(){
+    let isLogin = getCookie("isLogin");
+    let userName = getCookie("userName");
+    if(isLogin !== '1'){//未登录
+        window.location.href = "login.html";
+    }
+    else{
+        $(".userName").html("你好！"+userName);
+    }
+}
+function LogoutCheck(){
+
+}
+function Logout(){
+    setCookie("isLogin","0")
+    window.location.href = "login.html";
+}
+
+function LogoutShow(){
+    $(".logout-overlay").fadeTo(200, 0.5);
+    $(".logout-box").show();
+}
+
+function LogoutHide(){
+    $(".logout-overlay").fadeOut(200);
+    $(".logout-box").hide();
+}
